60fad7e2716ef427bb987acf6481b9fa02516ebb
TonyPepe <tonybear55665566@gmail.com>
2019-05-19 01:04:57 +0800

    P771

--- src/main/kotlin/com/tonypepe/leetcode/P771.kt
+++ src/main/kotlin/com/tonypepe/leetcode/P771.kt
@@ -2,10 +2,8 @@ package com.tonypepe.leetcode
 
 class P771 {
     fun numJewelsInStones(j: String, s: String): Int {
-        val jChars = j.toCharArray()
-        val sChars = s.toCharArray()
         var ans = 0
-        jChars.forEach { x ->
+        j.forEach { x ->
             s.forEach { y ->
                 if (x == y)
                     ans++
